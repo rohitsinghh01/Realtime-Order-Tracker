2bce375a307356750ee867b48790b33a36babc0c
Rohit <singh.rohitraj122@gmail.com>
2023-06-17 23:39:32 +0530

    some changes

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
+const path = require("path");
+const ejs = require("ejs");
+// const expressLayout = require("express-ejs-layouts");
 
+// app.use(expressLayout);
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
 
-
-
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 app.listen(port, () => {
   console.log(`Server started at ${port}`);
