8745fc92b483bbca958b8d360457c197c822acb3
Rohit <singh.rohitraj122@gmail.com>
2023-06-24 23:03:40 +0530

    ejs added

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
+
 const connectDb = require("./db/conn");
+
 const path = require("path");
 const ejs = require("ejs");
-// const expressLayout = require("express-ejs-layouts");
+var partials = require('express-partials');
 
-// app.use(expressLayout);
+const publicPath=path.join(__dirname,'/public')
+
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
+app.use(partials());
+
+app.use(express.static(publicPath))
+
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.get('/login',(req,res)=>{
+  res.render('auth/login')
+})
+app.get('/register',(req,res)=>{
+  res.render('auth/register')
+})
+
+app.get('/cart',(req,res)=>{
+  res.render('customer/cart')
+})
+
+
 app.listen(port, () => {
-  connectDb();
+  connectDb(process.env.MONGO_URI);
   console.log(`Server started at ${port}`);
 });
