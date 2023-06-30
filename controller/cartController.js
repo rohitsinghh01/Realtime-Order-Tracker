f2f44e6f2c4c4d1d50f718bc0560e95772617ce0
Rohit <singh.rohitraj122@gmail.com>
2023-06-30 23:07:44 +0530

    cart function added

--- controller/cartController.js
+++ controller/cartController.js
@@ -3,28 +3,32 @@ const cart = (req, res) => {
 };
 
 const updateCart = (req, res) => {
-  console.log(req.body)
-  // if (!req.session.cart) {
-  //   req.session.cart = {
-  //     items: {},
-  //     totalQty: 0,
-  //     totalPrice: 0,
-  //   };
-  // }
-  // let cart = req.session.cart;
-  // if (!cart.items[req.body._id]) {
-  //   cart.items[req.body._id] = {
-  //     item: req.body,
-  //     qty: 1,
-  //   };
-  //   cart.totalQty = cart.totalQty + 1;
-  //   cart.totalPrice = cart.totalPrice + req.body.price;
-  // } else {
-  //   cart.items[req.body._id].qty = cart.items[req.body._id].qty + 1;
-  //   cart.totalQty = cart.totalQty + 1;
-  //   cart.totalPrice = cart.totalPrice + req.body.price;
-  // }
-  // res.json({ totalQty: req.session.cart.totalQty });
+
+    if (!req.session) {
+      req.session = {};
+    }
+
+  if (!req.session.cart) {
+    req.session.cart = {
+      items: {},
+      totalQty: 0,
+      totalPrice: 0,
+    };
+  }
+  let cart = req.session.cart;
+  if (!cart.items[req.body._id]) {
+    cart.items[req.body._id] = {
+      item: req.body,
+      qty: 1,
+    };
+    cart.totalQty = cart.totalQty + 1;
+    cart.totalPrice = cart.totalPrice + req.body.price;
+  } else {
+    cart.items[req.body._id].qty = cart.items[req.body._id].qty + 1;
+    cart.totalQty = cart.totalQty + 1;
+    cart.totalPrice = cart.totalPrice + req.body.price;
+  }
+  res.json({ totalQty: req.session.cart.totalQty });
 };
 
 module.exports = { cart, updateCart };
--- public/script.js
+++ public/script.js
@@ -1,10 +1,29 @@
 // import axios from 'axios';
 let addToCart = document.querySelectorAll('.add-to-cart');
-// let cartCounter = document.querySelector('#cartCounter');
+let cartCounter = document.querySelector('#cartCounter');
 
 function updateCart(pizza) {
-  axios.post('update-cart').then((res) => {
-    // cartCounter.innerText=res.data.totalQty
+  axios.post('update-cart',pizza).then((res) => {
+    console.log(res.data.totalQty);
+    cartCounter.innerText=res.data.totalQty
+     toastr.info('Item added to cart');
+     toastr.options = {
+       closeButton: false,
+       debug: false,
+       newestOnTop: false,
+       progressBar: false,
+       positionClass: 'toast-top-right',
+       preventDuplicates: false,
+       onclick: null,
+       showDuration: '300',
+       hideDuration: '1000',
+       timeOut: '5000',
+       extendedTimeOut: '1000',
+       showEasing: 'swing',
+       hideEasing: 'linear',
+       showMethod: 'fadeIn',
+       hideMethod: 'fadeOut',
+     };
   });
 }
 
